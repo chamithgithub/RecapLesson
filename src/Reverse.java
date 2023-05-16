128ece10684561d07c3689f16fc29d4e04e40d77
Cha1552164 <chamithgithub>
2023-05-16 22:30:48 +0530

    method finished

--- src/Reverse.java
+++ src/Reverse.java
@@ -7,13 +7,23 @@ public class Reverse {
 
 //        System.out.println(name.charAt(0));
 
-        int length = name.length();
+//        int length = name.length();
+//
+//        for(int i=length-1; i>=0 ; i--){
+//            reverse =reverse+name.charAt(i);
+//        }
+//        System.out.println(reverse);
 
-        for(int i=length-1; i>=0 ; i--){
-            reverse =reverse+name.charAt(i);
+        //method 2
+        //charactorArray
+
+        char[] chars = name.toCharArray();
+        int length = chars.length;
+
+        for (int i=length-1 ; i>=0; i--){
+            reverse=reverse+chars[i];
         }
         System.out.println(reverse);
-
 
     }
 }
