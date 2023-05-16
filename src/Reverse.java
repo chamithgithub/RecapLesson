ea6030b50c7fe2c34dca14a1ea4ef3494b39672b
Cha1552164 <chamithgithub>
2023-05-16 22:20:46 +0530

    new lesson

---
+++ src/Reverse.java
@@ -0,0 +1,29 @@
+public class Reverse {
+    public static void main(String[] args) {
+        //method 1
+        //len and chartAt
+        String name ="Chamith";
+        String reverse="";
+
+//        System.out.println(name.charAt(0));
+
+//        int length = name.length();
+//
+//        for(int i=length-1; i>=0 ; i--){
+//            reverse =reverse+name.charAt(i);
+//        }
+//        System.out.println(reverse);
+
+        //method 2
+        //charactorArray
+
+        char[] chars = name.toCharArray();
+        int length = chars.length;
+
+        for (int i=length-1 ; i>=0; i--){
+            reverse=reverse+chars[i];
+        }
+        System.out.println(reverse);
+
+    }
+}
