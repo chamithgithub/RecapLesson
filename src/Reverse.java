27e12b2a55b8be3663edf22a3c46b4cfdf9b938a
Cha1552164 <chamithgithub>
2023-05-16 22:27:40 +0530

    create variable

--- src/Reverse.java
+++ src/Reverse.java
@@ -5,25 +5,8 @@ public class Reverse {
         String name ="Chamith";
         String reverse="";
 
-//        System.out.println(name.charAt(0));
 
-//        int length = name.length();
-//
-//        for(int i=length-1; i>=0 ; i--){
-//            reverse =reverse+name.charAt(i);
-//        }
-//        System.out.println(reverse);
 
-        //method 2
-        //charactorArray
-
-        char[] chars = name.toCharArray();
-        int length = chars.length;
-
-        for (int i=length-1 ; i>=0; i--){
-            reverse=reverse+chars[i];
-        }
-        System.out.println(reverse);
 
     }
 }
